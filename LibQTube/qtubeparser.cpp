cc384254e48b28d2afbcb21de9912fd73f2681c6
calvarez <calvarez@orange-vallee.net>
2011-10-08 21:20:28 +0200

    Create a video object for each parsed video

--- LibQTube/qtubeparser.cpp
+++ LibQTube/qtubeparser.cpp
@@ -1,75 +1,89 @@
 #include "qtubeparser.h"
 #include <QDateTime>
 #include <iostream>
+#include "user.h"
+
 using namespace std;
 QTubeParser::QTubeParser()
 {
 }
-void QTubeParser::parseFeed(QIODevice *pIoDevice) {
+void QTubeParser::parseFeed(QIODevice &pIoDevice) {
     cout << "parseFeed... " <<endl;
     QDomDocument doc;
     QString error;
     int errorLine;
     int errorColumn;
-    if(!doc.setContent(pIoDevice, true, &error, &errorLine, &errorColumn)) {
+    if(!doc.setContent(&pIoDevice, true, &error, &errorLine, &errorColumn)) {
         cout << "!bad content at "<< errorLine << ":" << errorColumn << ": " << (error.toAscii().data()) <<endl;
         return;
     }
     QDomElement docElem = doc.documentElement();
     QDomNodeList itemElems = docElem.elementsByTagName("item");
     for(int i=0; i < itemElems.count(); i++) {
-        QString *author = NULL;
-        QString *title = NULL;
-        QString *videoId = NULL;
-        qreal rating = 0.0f;
-        QDateTime * uploadedDate = NULL;
-        qint32 duration = 0;
 
         QDomElement itemElem = itemElems.at(i).toElement();
-        title = getSubElementText(itemElem, "title");
-        author = getSubElementText(itemElem, "author");
-        QDomNodeList mediaGroupElems = itemElem.elementsByTagName("group");
-        if(!mediaGroupElems.isEmpty()) {
-            QDomElement mediaGroupElem = mediaGroupElems.at(0).toElement();
-            videoId = getSubElementText(mediaGroupElem, "videoid");
-            QString *dateString = getSubElementText(mediaGroupElem, "uploaded");
-
-            QDomNodeList durationElems = mediaGroupElem.elementsByTagName("duration");
-            if(!durationElems.isEmpty()) {
-                QDomElement durationElem = durationElems.at(0).toElement();
-                QString durationString = durationElem.attribute("seconds");
-                duration = durationString.toInt();
-            }
-        }
-        QDomNodeList ratingElems = itemElem.elementsByTagName("rating");
-        if(!ratingElems.isEmpty()) {
-            QDomElement ratingElem = ratingElems.at(0).toElement();
-            QString ratingString = ratingElem.attribute("average", "0.0");
-            rating = (qreal) ratingString.toFloat();
-
-        }
-        if(title != NULL)
-            cout << "video title: " << (title->toAscii().data()) << endl;
-
-        if(author != NULL)
-            cout << "  author: " << author->toAscii().data() << endl;
-
-        if(videoId != NULL)
-            cout << "  video id: " << videoId->toAscii().data() << endl;
-
-        cout << "  rating: " << rating <<endl;
-        cout << "  duration: " << duration << endl;
+        Video *video = parseVideo(itemElem);
     }
 }
 
-QString* QTubeParser::getSubElementText(QDomElement pElement, QString pSubElementName) {
+QString QTubeParser::getSubElementText(QDomElement pElement, QString pSubElementName) {
     QDomNodeList subElems = pElement.elementsByTagName(pSubElementName);
     for(int i=0; i < subElems.count(); i++) {
         QDomElement subElem = subElems.at(i).toElement();
         QString text = subElem.text();
-        return new QString(text);
+        return text;
     }
-    return new QString("");
+    QString empty("");
+    return empty;
 }
 
+Video *QTubeParser::parseVideo(QDomElement pElement) {
+    QString author;
+    QString title;
+    QString videoId;
+    qreal rating = 0.0f;
+    qint32 duration = 0;
+    QString uploadedDate;
+    title = getSubElementText(pElement, "title");
+    author = getSubElementText(pElement, "author");
+    QDomNodeList mediaGroupElems = pElement.elementsByTagName("group");
+    if(!mediaGroupElems.isEmpty()) {
+        QDomElement mediaGroupElem = mediaGroupElems.at(0).toElement();
+        videoId = getSubElementText(mediaGroupElem, "videoid");
+        uploadedDate = getSubElementText(mediaGroupElem, "uploaded");
+
+        QDomNodeList durationElems = mediaGroupElem.elementsByTagName("duration");
+        if(!durationElems.isEmpty()) {
+            QDomElement durationElem = durationElems.at(0).toElement();
+            QString durationString = durationElem.attribute("seconds");
+            duration = durationString.toInt();
+        }
+    }
+    QDomNodeList ratingElems = pElement.elementsByTagName("rating");
+    if(!ratingElems.isEmpty()) {
+        QDomElement ratingElem = ratingElems.at(0).toElement();
+        QString ratingString = ratingElem.attribute("average", "0.0");
+        rating = (qreal) ratingString.toFloat();
+
+    }
+    if(title != NULL)
+        cout << "video title: " << (title.toAscii().data()) << endl;
+
+    if(author != NULL)
+        cout << "  author: " << author.toAscii().data() << endl;
+
+    if(videoId != NULL)
+        cout << "  video id: " << videoId.toAscii().data() << endl;
+
+    cout << "  rating: " << rating <<endl;
+    cout << "  duration: " << duration << endl;
+    Video * result = new Video(videoId);
+    User *user = new User(author);
+    result->setAuthor(user);
+    result->setName(title);
+    result->setDuration(duration);
+    result->setRating(rating);
+    result->setPubDate(uploadedDate);
+    return result;
+}
 
--- LibQTube/qtubeparser.h
+++ LibQTube/qtubeparser.h
@@ -2,14 +2,16 @@
 #define QTUBEPARSER_H
 
 #include <QDomDocument>
+#include "video.h"
 
 class QTubeParser
 {
 public:
     QTubeParser();
-    void parseFeed(QIODevice *);
+    void parseFeed(QIODevice &);
 private:
-    QString* getSubElementText(QDomElement pElement, QString pSubElementName);
+    QString getSubElementText(QDomElement pElement, QString pSubElementName);
+    Video* parseVideo(QDomElement pElement);
 };
 
 #endif // QTUBEPARSER_H
